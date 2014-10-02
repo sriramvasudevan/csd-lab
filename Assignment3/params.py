57d5f6ec9a263df6887e8531b9aa572f79dd7c0f
Sriram V <sriram.vasudevan.93@gmail.com>
2014-10-02 13:36:03 +0530

    Added global params file

---
+++ Assignment3/params.py
@@ -0,0 +1,13 @@
+# Global Vars
+NUM_RS=1
+NUM_FU=2
+NUM_TYPES_INSTR=9
+MAX_REORDER_BUFFER=128
+MAX_STORE_BUFFER=8 
+MAX_RESERVATION_STATION=128
+MAX_REGISTERS=8
+MAX_INSTR=1
+
+global_ins_counter = 0
+store_counter = 0
+global_time = 0
